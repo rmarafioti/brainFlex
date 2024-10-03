1828946b65bcb2ed880887050a1f5683f043c073
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-10-02 23:26:25 -0500

    Fix Vite output directory and Express static file serving

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // ensure the output directory is correctly set to 'dist'
+  },
+});
