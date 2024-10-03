0178e02e550f869d4d1b77a7ba6a02ea21b1800b
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-10-03 09:20:11 -0500

    reverted files back to original state

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})*/
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // ensure the output directory is correctly set to 'dist'
-  },
 });
