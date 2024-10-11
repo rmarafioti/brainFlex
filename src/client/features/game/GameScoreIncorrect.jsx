8e6bb223cfb04b2da7f83512dec49b6d9a82e8d7
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-10-10 20:26:12 -0500

    restyled game score incorrect page

--- src/client/features/game/GameScoreIncorrect.jsx
+++ src/client/features/game/GameScoreIncorrect.jsx
@@ -1,18 +1,15 @@
 import { Link } from "react-router-dom";
 import { useGetMeQuery } from "../account/accountSlice";
 import React from "react";
-import {
-  useGetImageWordQuery,
-  useGetGameQuery,
-} from "./gameSlice";
+import { useGetImageWordQuery, useGetGameQuery } from "./gameSlice";
 
 import "./game.css";
 
 const isThereAQuiz = () => {
-  const {data: me} = useGetMeQuery();
+  const { data: me } = useGetMeQuery();
   let foundQuizToday = false;
-//isfoundquizcompleted is if it finds a quiz for that user today, is that quiz completed or not
-let todaysQuiz = null;
+  //isfoundquizcompleted is if it finds a quiz for that user today, is that quiz completed or not
+  let todaysQuiz = null;
   //this function looks at all of a users quizzes, then for each of them takes the datetime of that quiz
   // and converts it to month date year format, then compares it to todays date in the same format
   // it then for each of the quizes if it finds a quiz, sets foundquiz today to true, and then after that sees if that quiz is completed.
@@ -27,8 +24,7 @@ let todaysQuiz = null;
   }
 
   return todaysQuiz;
-
-}
+};
 
 /**
  *
@@ -42,16 +38,17 @@ export default function GameScoreIncorrect() {
   // do we need this? const [updateUser] = useUpdatedUserMutation();
 
   return (
-    <>
+    <article className="scoreIncorrect">
       <section id="imageContainer">
         <img id="image" src={imageandword?.image_url} />
       </section>
       <article id="answerInformation">
-        <div id="correctAnswer">
-          <h1>The Correct Word Was: {imageandword?.topic_word}</h1>
+        <div>
+          <h1 id="correctAnswer">The Correct Word Was:</h1>
+          <h2 className="correctAnswerWord">{imageandword?.topic_word}</h2>
         </div>
-        <div id="quizScore">
-          <p>FINAL QUIZ SCORE: 0/10</p>
+        <div>
+          <p id="quizScore">FINAL QUIZ SCORE: 0/10</p>
         </div>
       </article>
       <button id="buttonGame">
@@ -59,6 +56,6 @@ export default function GameScoreIncorrect() {
           HOME
         </Link>
       </button>
-    </>
+    </article>
   );
 }
